1637000447d2cf11998126a2af2584f68ec08bbd
Joe <joe.bell34@btinternet.com>
2023-09-18 18:02:06 +0100

    Library: New bitset and hashtable types.

---
+++ kernel/include/library/bitset.h
@@ -0,0 +1,142 @@
+/*
+ * bekOS is a basic OS for the Raspberry Pi
+ * Copyright (C) 2023 Bekos Contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef BEKOS_BITSET_H
+#define BEKOS_BITSET_H
+
+#include "assertions.h"
+#include "buffer.h"
+#include "optional.h"
+#include "types.h"
+
+namespace bek {
+
+class bitset_view {
+public:
+    bitset_view(char* data, uSize length) : m_data{data}, m_length{length} {}
+    explicit bitset_view(bek::mut_buffer data) : m_data{data.data()}, m_length{data.size() * 8} {}
+
+    [[nodiscard]] bool get(uSize index) const {
+        ASSERT(index < m_length);
+        return (m_data[index / 8] & (1u << (index % 8))) != 0;
+    }
+
+    void set(uSize index, bool val) {
+        ASSERT(index < m_length);
+        if (val) {
+            m_data[index / 8] |= (1u << (index % 8));
+        } else {
+            m_data[index / 8] &= ~(1u << (index % 8));
+        }
+    }
+
+    void set_range(uSize start, uSize count, bool val) {
+        ASSERT(start + count < m_length);
+        uSize end = start + count;
+        // Align Start
+        while (start % 8 && start < end) {
+            set(start++, val);
+        }
+
+        // Align End
+        while (end & 8 && start < end) {
+            set(--end, val);
+        }
+
+        // Now aligned, so do full
+        while (start < end) {
+            m_data[start / 8] = (val) ? 0xFF : 0x00;
+            start += 8;
+        }
+    }
+
+    uSize get_region_size(uSize start_index, bool val, uSize max_size = 0xFFFFFFFFFFFFFFFF) const {
+        uSize region_max_size = bek::min(max_size, m_length - start_index);
+        uSize end             = start_index + region_max_size;
+        uSize aligned_end     = (end / 8) * 8;
+        uSize start           = start_index;
+        bool cease            = false;
+        // Align Start
+        while (start % 8 && start < end) {
+            if (get(start) != val) {
+                cease = true;
+                break;
+            }
+            start++;
+        }
+        if (cease) return start - start_index;
+
+        while (start < aligned_end) {
+            if (m_data[start / 8] != (val ? 0xFF : 0x00)) {
+                break;
+            }
+            start += 8;
+        }
+
+        // Either, we've ceased prematurely (byte not uniform) or ceased to do unaligned checking at
+        // end.
+        while (start < end) {
+            if (get(start) != val) {
+                break;
+            }
+            start++;
+        }
+        return start - start_index;
+    }
+
+    struct fit_result_t {
+        uSize index;
+        uSize size;
+
+        constexpr static fit_result_t invalid() { return {0, 0}; }
+
+        [[nodiscard]] constexpr bool is_invalid() const { return (index == 0) && (size == 0); }
+    };
+
+    [[nodiscard]] fit_result_t find_first_fit(uSize length, bool val, uSize hint = 0,
+                                              uSize alignment = 1, uSize alignment_offset = 0,
+                                              uSize max_size = 0xFFFFFFFFFFFFFFFF) const {
+        // Align-up hint.
+        VERIFY(alignment_offset < alignment);
+        uSize start_index = align_up(hint + alignment_offset, alignment) - alignment_offset;
+        // Search in units of alignment
+        while (start_index < m_length) {
+            if (get(start_index) == val) {
+                uSize region_size = get_region_size(start_index, val, max_size);
+                if (region_size >= length) {
+                    return {start_index, region_size};
+                }
+                start_index += region_size;
+            } else {
+                uSize bad_region_size = get_region_size(start_index, !val);
+                bad_region_size       = bek::align_up(bad_region_size, alignment);
+                start_index += bad_region_size;
+            }
+        }
+        return fit_result_t::invalid();
+    }
+
+private:
+    char* m_data;
+    /// In bits.
+    uSize m_length;
+};
+
+}  // namespace bek
+
+#endif  // BEKOS_BITSET_H
---
+++ kernel/include/library/hashtable.h
@@ -0,0 +1,177 @@
+/*
+ * bekOS is a basic OS for the Raspberry Pi
+ * Copyright (C) 2023 Bekos Contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef BEKOS_HASHTABLE_H
+#define BEKOS_HASHTABLE_H
+
+#include "kstring.h"
+#include "mm/kmalloc.h"
+#include "types.h"
+#include "utility.h"
+namespace bek {
+
+namespace detail {
+template <typename T>
+concept has_bek_hash_overload = requires(const T& t) {
+    { bek::hash(t) } -> same_as<u64>;
+};
+}
+
+template <typename T>
+struct hasher;
+
+template <detail::has_bek_hash_overload T>
+struct hasher<T> {
+    u64 operator()(const T& x) const noexcept { return bek::hash(x); }
+};
+
+template <typename Key, typename Val, typename Hasher = bek::hasher<Key>>
+class hashtable {
+public:
+    using Pair     = pair<Key, Val>;
+    using Iterator = Pair*;
+
+    explicit hashtable(uSize capacity = 4)
+        : buckets{nullptr}, filled{nullptr}, capacity{capacity}, load{0} {
+        buckets = reinterpret_cast<Pair*>(kmalloc(capacity * sizeof(Pair)));
+        filled  = reinterpret_cast<bool*>(kmalloc(capacity * sizeof(bool)));
+        memset(filled, 0, capacity * sizeof(bool));
+    };
+
+    pair<Iterator, bool> insert(const Pair& pair)
+        requires copy_constructible<Pair>
+    {
+        check_size();
+        return unchecked_set(pair, false);
+    }
+
+    pair<Iterator, bool> insert(Pair&& pair) {
+        check_size();
+        return unchecked_set(bek::move(pair), false);
+    }
+
+    void set(Key key, Val val) {
+        check_size();
+        return unchecked_set({key, val}, true);
+    }
+
+    Val* find(const Key& key) {
+        auto index = unchecked_lookup(key);
+        if (filled[index]) {
+            return &(buckets[index].second);
+        } else {
+            return nullptr;
+        }
+    }
+
+    const Val* find(const Key& key) const {
+        auto index = unchecked_lookup(key);
+        if (filled[index]) {
+            return &(buckets[index].second);
+        } else {
+            return nullptr;
+        }
+    }
+
+    const Val& find_unchecked(const Key& key) const { return *find(key); }
+
+    Val& find_unchecked(const Key& key) { return *find(key); }
+
+private:
+    void check_size() {
+        if (load * expandThreshold > capacity) {
+            auto new_size = capacity * expandFactor;
+            // Need to expand + rehash
+            auto new_buckets = reinterpret_cast<Pair*>(kmalloc(new_size * sizeof(Pair)));
+            auto new_filled  = reinterpret_cast<bool*>(kmalloc(new_size * sizeof(bool)));
+            memset(new_filled, 0, new_size * sizeof(bool));
+
+            // Swap
+            auto old_buckets  = buckets;
+            buckets           = new_buckets;
+            auto old_filled   = filled;
+            filled            = new_filled;
+            auto old_capacity = capacity;
+            capacity          = new_size;
+            auto old_load     = load;
+            load              = 0;
+
+            // Transfer
+            for (uSize i = 0; i < old_capacity; i++) {
+                if (old_filled[i]) {
+                    unchecked_set(bek::move(old_buckets[i]), false);
+                    old_buckets[i].~Pair();
+                }
+            }
+
+            // Delete
+            kfree(old_buckets, old_capacity * sizeof(Pair));
+            kfree(old_filled, old_capacity * sizeof(bool));
+        }
+    }
+
+    pair<Iterator, bool> unchecked_set(Pair&& pair, bool overwrite) {
+        auto index  = unchecked_lookup(pair.first);
+        Iterator it = &buckets[index];
+        if (filled[index]) {
+            if (overwrite) {
+                *it = bek::move(pair);
+                return {it, true};
+            } else {
+                return {it, false};
+            }
+        } else {
+            new (it) Pair{bek::move(pair)};
+        }
+        load += filled[index] ? 0 : 1;
+        filled[index] = true;
+        return {it, true};
+    }
+
+    /// Looks for Key. If found, returns index. If not, returns appropriate insertion point.
+    uSize unchecked_lookup(const Key& key) const {
+        auto hash   = Hasher{}(key);
+        uSize index = hash % capacity;
+        for (uSize i = index; i < capacity; i++) {
+            if (!filled[i]) return i;
+            if (key == buckets[i].first) return i;
+        }
+        for (uSize i = 0; i < index; i++) {
+            if (!filled[i]) return i;
+            if (key == buckets[i].first) return i;
+        }
+        ASSERT(0);
+    }
+
+    static constexpr int expandThreshold = 2;
+    static constexpr int expandFactor    = 2;
+
+    /// Dynamically Allocated array of buckets.
+    Pair* buckets;
+    /// Dynamically Allocated bitmap of filled flags.
+    bool* filled;
+
+    /// Length of array allocated
+    uSize capacity;
+    /// How many items?
+    uSize load;
+};
+
+};  // namespace bek
+
+#endif  // BEKOS_HASHTABLE_H
