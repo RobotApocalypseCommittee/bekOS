32c38c792b7a10e49494ef057b14ab1e3361608a
Joe Bell <joe.bell34@btinternet.com>
2024-09-07 17:19:21 +0100

    Tooling: Better qemu runner script.

--- tools/run-qemu.sh
+++ tools/run-qemu.sh
@@ -1,7 +1,7 @@
-#!/bin/zsh
+#!/bin/sh
 #
 # bekOS is a basic OS for the Raspberry Pi
-# Copyright (C) 2023 Bekos Contributors
+# Copyright (C) 2024 Bekos Contributors
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -17,6 +17,54 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 
-echo Starting Qemu
+print_help()
+{
+  NAME=$(basename "$0")
+  echo "Qemu runner for bekOS."
+  echo
+  echo "Usage: $NAME MACHINE EXECUTABLE [QEMU_CMD] [-d]"
+  echo "  Supported MACHINEs: virt, rpi3."
+  echo "  EXECUTABLE: The kernel image."
+  echo "  QEMU_CMD: qemu command to run; if not set defaults to qemu-system-aarch64"
+  each "  -d: Debug enabled. If set, GDB debug server will be enabled at port 1234 and machine paused."
+}
 
-"$1" -M raspi3b -serial null -serial mon:stdio -d guest_errors,unimp -usb -device usb-kbd -device usb-mouse -kernel $2
+usage ()
+{
+  print_help
+  exit 1
+}
+
+[ -n "$1" ] || usage
+
+QEMU_ARGS=()
+
+case "$1" in
+  virt)
+    QEMU_ARGS+=(-machine virt -cpu cortex-a57 -usb -device qemu-xhci -device usb-kbd -device virtio-gpu-device -device "virtio-blk-device,drive=main_drive" -drive "format=raw,id=main_drive,file=fat:system,readonly=on,if=none" -serial mon:stdio -global virtio-mmio.force-legacy=false)
+    ;;
+  rpi3)
+    QEMU_ARGS+=(-M raspi3b -serial null -serial mon:stdio -d "guest_errors,unimp" -usb -device usb-kbd -device usb-mouse)
+    ;;
+  *)
+    echo "Unknown MACHINE: $1"
+    exit 1
+    ;;
+esac
+
+if [ "$3" = "-d" ] || [ "$4" = "-d" ]
+then
+  QEMU_ARGS+=(-s -S)
+fi
+
+if [ -n "$3" ] && [ "$3" != "-d" ]
+then
+  QEMU_CMD="$3"
+else
+  QEMU_CMD="qemu-system-aarch64"
+fi
+
+QEMU_ARGS+=("-kernel" "$2")
+
+echo "Running $QEMU_CMD ${QEMU_ARGS[*]}"
+"$QEMU_CMD" "${QEMU_ARGS[@]}"
